ee0d020b036402daa1a718c7cb5746418242c674
goldevan66-66navedlog <goldevan66grad@gmail.com>
2026-06-07 21:22:52 -0500

    Add: search-for-word-ii - submission-9

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-9.py
@@ -0,0 +1,118 @@
+# class TrieNode:
+#     def __init__(self):
+#         self.children = {}
+#         self.word = False
+#     def addword(self,w):
+#         root = self
+#         for c in w:
+#             if(c not in root.children):
+#                 root.children[c] = TrieNode()
+#             root = root.children[c]
+#         root.word = True
+
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+    
+    def addWord(self,word):
+        curr = self
+        for c in word:
+            if(c not in curr.children):
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isWord = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+        
+        ROWS, COLS = len(board), len(board[0])
+        res, visited = set(), set()
+
+        def dfs(r,c,node,word):
+            if((r<0) or (c<0) or (r>=ROWS) or (c>=COLS) or ((r,c) in visited) or (board[r][c] not in node.children)):
+                return
+            visited.add((r,c))
+            word  = word + board[r][c]
+            node = node.children[board[r][c]]
+
+            if(node.isWord):
+                res.add(word)
+            
+            dfs(r-1,c,node,word)
+            dfs(r+1,c,node,word)
+            dfs(r,c-1,node,word)
+            dfs(r,c+1,node,word)
+
+            visited.remove((r,c))
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,root,"")
+        return list(res)
+
+
+
+
+
+
+
+
+
+
+        # trie = TrieNode()
+        # for w in words:
+        #     trie.addword(w)
+        
+        # ROWS, COLS = len(board), len(board[0])
+        # res, visited = set(), set()
+
+        # def dfs(i,j,node, word):
+        #     if(i < 0 or i >= ROWS or j < 0 or j >= COLS or (i,j) in visited or board[i][j] not in node.children):
+        #         return 
+
+        #     visited.add((i,j))
+        #     node = node.children[board[i][j]]
+        #     word += board[i][j]
+        #     if(node.word):
+        #         res.add(word)
+            
+        #     dfs(i+1,j,node,word)
+        #     dfs(i-1,j,node,word)
+        #     dfs(i,j+1,node,word)
+        #     dfs(i,j-1,node,word)
+        #     visited.remove((i,j))
+        
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         dfs(r,c,trie,"")
+        # return list(res)
+        # ROWS, COLS = len(board), len(board[0])
+        # res = []
+        # woords = set(words)
+        # def dfs(i,j,word,target,k):
+        #     if(i >= ROWS or i < 0 or j >= COLS or j < 0 or board[i][j]=="*" or len(word)==len(target) or board[i][j]!=target[k]):
+        #         copy = "".join(word) 
+        #         if(copy in woords):
+        #             woords.remove(copy)
+        #             res.append(copy)
+        #         return
+        #     c = board[i][j]    
+        #     word.append(c)
+        #     board[i][j] = "*"
+        #     dfs(i-1,j,word,target,k+1)
+        #     dfs(i+1,j,word,target,k+1)
+        #     dfs(i,j-1,word, target, k+1)
+        #     dfs(i,j+1,word, target, k+1)
+        #     board[i][j] = c
+        #     word.pop()
+        #     return
+        
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         for w in words:
+        #             dfs(i,j,[],w,0)
+        # return res
