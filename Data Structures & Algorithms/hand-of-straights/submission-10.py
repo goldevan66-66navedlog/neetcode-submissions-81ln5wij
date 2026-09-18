41eeb420c12d34c30eb4c7a39cf6264bac62a003
goldevan66-66navedlog <goldevan66grad@gmail.com>
2026-09-18 02:55:57 -0500

    Add: hand-of-straights - submission-10

---
+++ Data Structures & Algorithms/hand-of-straights/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if(len(hand)%groupSize != 0):
+            return False
+        
+        groupNum = len(hand)//groupSize
+        hand.sort() # sorts the hand in incresing order
+
+        lst = [[] for i in range(groupNum)]
+        prev = -1
+        group = -1
+        nextAvail = 0
+        for i in range(len(hand)):
+            if(prev != hand[i]):
+                group = nextAvail
+            else:
+                group += 1
+
+            if(group >= groupNum or (len(lst[group])>0 and hand[i]-lst[group][-1]!=1)):
+                return False
+
+            lst[group].append(hand[i])
+
+            if(len(lst[group])==groupSize):
+                nextAvail +=1
+
+            prev = hand[i]
+        
+        
+        return True
+
+            
